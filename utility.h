06496d0d5fef63fcdf8f2944029a7983116671fd
Daniel Collins <dc_calnet@berkeley.edu>
2020-08-24 22:12:07 -0700

    fixed assert invocation

--- utility.h
+++ utility.h
@@ -1,12 +1,17 @@
 #ifndef UTILITY_H
 #define UTILITY_H
 
+/* Used for tools that can analyze code w/ recursion bounds. */
+#ifndef uassert
+#define uassert(x)
+#endif /* !assert */
+
 /* Gives explicit recursion bound. */
 static void  _itoa (char** cursor, long long value, unsigned n) {
 	/* Math is done with negative numbers. */
 	/* This way, we can deal with INT_MIN. */
 
-	if ( n  >  20 ) return assert ( 0 ); /* 20: Digits in 2**64. */
+	if ( n  >  20 ) return uassert ( 0 ); /* 20: Digits in 2**64. */
 	if (value < -9)
 		_itoa (cursor, value / 10, n+1);
 
@@ -30,5 +35,7 @@ static void  sadd (char** cursor, const char* str) {
 static void* mcpy (void*  destin, const void* src, unsigned n) {
 	while (n--) *((char*)destin) = *((char*)src); return destin;
 }
+
+#undef uassert
 
 #endif /* UTILITY_H */
